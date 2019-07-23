5f47b057f0a46493f7d3b9c66c4b928a98ec130a
cspence8 <cspence8@jaguarlandrover.com>
2019-07-23 21:50:26 +0100

    chore: generate spring default project

---
+++ src/main/java/com/spenceuk/cardealer/CarDealerBackEndServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spenceuk.cardealer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealerBackEndServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDealerBackEndServiceApplication.class, args);
+	}
+
+}
