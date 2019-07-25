22eafaa91dfa72b0e02b53ef0099159467d56bb7
cspence8 <cspence8@jaguarlandrover.com>
2019-07-25 09:23:16 +0100

    fix: checkstyle settings for vscode and checkstyle extension

--- src/main/java/com/spenceuk/cardealer/CarDealerBackEndServiceApplication.java
+++ src/main/java/com/spenceuk/cardealer/CarDealerBackEndServiceApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarDealerBackEndServiceApplication {
 
+  /**
+   * Main method for the application.
+   *
+   * @param args varags String values from the commandline.
+   */
   public static void main(String... args) {
     SpringApplication.run(CarDealerBackEndServiceApplication.class, args);
   }
