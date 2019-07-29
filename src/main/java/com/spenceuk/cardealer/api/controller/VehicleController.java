638160076497f51bd794da27cd935acf95909014
cspence8 <cspence8@jaguarlandrover.com>
2019-07-29 19:11:48 +0100

    feat: update method for vehicles to use with PUT requests

--- src/main/java/com/spenceuk/cardealer/api/controller/VehicleController.java
+++ src/main/java/com/spenceuk/cardealer/api/controller/VehicleController.java
@@ -1,6 +1,10 @@
 package com.spenceuk.cardealer.api.controller;
 
+import static com.spenceuk.cardealer.api.exception.ApiExceptions.ID_MISMATCH;
+import static org.springframework.http.HttpStatus.CREATED;
+
 import com.spenceuk.cardealer.api.dto.VehicleDto;
+import com.spenceuk.cardealer.api.exception.ApiException;
 import com.spenceuk.cardealer.service.VehicleService;
 
 import java.util.List;
@@ -9,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,14 +44,24 @@ public class VehicleController {
    */
   @PostMapping
   ResponseEntity<VehicleDto> newVehicle(@RequestBody VehicleDto newVehicle) {
-    return ResponseEntity.status(201)
+    return ResponseEntity.status(CREATED)
         .body(service.saveVehicle(newVehicle));
   }
 
-  @PutMapping
-  ResponseEntity<VehicleDto> updateVehicle(@RequestBody VehicleDto update) {
-    
-    return null;
+  /**
+   * Update a specific vehicles details.
+   * @param updateDto Object contaning values to update.
+   * @return 200 OK
+   */
+  @PutMapping("/{id}")
+  ResponseEntity<Void> updateVehicle(@RequestBody VehicleDto updateDto,
+                                     @PathVariable("id") Long id) {
+
+    if (id.longValue() != updateDto.getId()) {
+      throw ApiException.idMismatch(id.longValue(), updateDto.getId());
+    }
+    service.updateAll(updateDto);
+    return ResponseEntity.ok().build();
   }
 
 }
